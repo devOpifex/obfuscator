d287552eaa6f63aabcb3017a7da1216c3a81c4c3
John Coene <jcoenep@gmail.com>
2024-12-07 03:42:30 +0100

    feat: write with dir creation

--- write.go
+++ write.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func writeString(file, code, header string) error {
@@ -20,4 +22,18 @@ func writeString(file, code, header string) error {
 	_, err = f.WriteString(code)
 
 	return err
+}
+
+func WriteWithDirCreate(path string, content string) error {
+	dir := filepath.Dir(path)
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("failed to create directory %s: %w", dir, err)
+	}
+
+	if err := os.WriteFile(path, []byte(content), 0644); err != nil {
+		return fmt.Errorf("failed to write file %s: %w", path, err)
+	}
+
+	return nil
 }
