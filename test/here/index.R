4e31e605f5e983bb0c56457c9268af77590769db
John Coene <jcoenep@gmail.com>
2024-12-08 05:18:16 +0100

    feat: transpiler write

--- test/here/index.R
+++ test/here/index.R
@@ -3,12 +3,7 @@
 # this is a comment
 # this is a comment
 # this is a comment
-`%||%` <- function(x, y) {
-  if (is.null(x)) y else x
-}
-
 get_home <- \(req, res) {
-  1 %||% 2
   x <- list(title = "Hello, World!")
   res$send(x$title)
 }
